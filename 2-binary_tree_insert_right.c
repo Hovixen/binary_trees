84f0e92298c5bb045ed200ad6603291ff83cbddd
logmar98 <marwanarbib15@gmail.com>
2024-01-03 21:55:15 +0100

    update task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,7 +7,7 @@
  * Return: returns pointer to the created node or NULL when if fail
  */
 
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
 
